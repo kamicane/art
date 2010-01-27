bf30d5c948679ad6c9f9607da9b34eb0f099b1a7
Valerio Proietti <kamicane@gmail.com>
2010-01-27 16:36:47 +0100

    - got rid of the icons (some were broken, but icons really do not belong here).

--- Source/ART.Shape.js
+++ Source/ART.Shape.js
@@ -117,43 +117,7 @@ ART.defineShapes({
 	},
 	
 	pill: function(size){
-		var m = size.x < size.y ? 'x' : 'y', r = size[m] / 2;
-		this.shape('rounded-rectangle', {x: size.x, y: size.y}, r);
+		this.shape('rounded-rectangle', {x: size.x, y: size.y}, size[(size.x < size.y) ? 'x' : 'y'] / 2);
 	}
 	
-});
-
-// Some icons. These need to be stroked rather than filled.
-
-ART.defineShape('plus-icon', function(size){
-	this.moveBy({x: 0, y: (size.y / 2)});
-	this.lineBy({x: size.x, y: 0});
-	this.moveBy({x: -(size.x / 2), y: -(size.y / 2)});
-	this.lineBy({x: 0, y: size.y});
-});
-
-ART.defineShape('resize-icon', function(size){
-	this.moveBy({x: size.x, y: 0});
-	this.lineBy({x: -size.x, y: size.y});
-});
-
-ART.defineShape('minus-icon', function(size){
-	this.moveBy({x: 0, y: (size.y / 2)});
-	this.lineBy({x: size.x, y: 0});
-});
-
-ART.defineShape('search-icon', function(size, ratio){
-	ratio = ratio || 0.8;
-	var max = ratio, min = 1 - ratio;
-	this.shape('ellipse', {x: size.x * max, y: size.y * max});
-	var lift = {x: -(size.x * min) / 2, y: -(size.y * min) / 2};
-	this.moveBy(lift);
-	this.lineBy({x: (size.x * min) - lift.x, y: (size.y * min) - lift.y});
-});
-
-ART.defineShape('close-icon', function(size){
-	this.moveBy({x: size.x, y: 0});
-	this.lineBy({x: -size.x, y: size.y});
-	this.moveBy({x: 0, y: -size.y});
-	this.lineBy({x: size.x, y: size.y});
 });
