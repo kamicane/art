78cfed210db6d41049c43bd11deb58ca71242ced
Valerio Proietti <kamicane@gmail.com>
2010-02-25 01:24:36 +0100

    - added simple show / hide methods.  - the structure of ART.VML / ART.SVG is now the same  - made a couple of function private.  - you can now set stroke-width, join, cap.

--- Source/ART.VML.js
+++ Source/ART.VML.js
@@ -101,6 +101,8 @@ ART.VML.Element = new Class({
 		return this;
 	},
 	
+	// transformations
+	
 	translate: function(x, y){
 		this.transform.translate = [x, y];
 		this._transform();
@@ -110,6 +112,18 @@ ART.VML.Element = new Class({
 	scale: function(x, y){
 		this.transform.scale = [x, y];
 		this._transform();
+		return this;
+	},
+	
+	// visibility
+	
+	hide: function(){
+		this.element.style.display = 'none';
+		return this;
+	},
+	
+	show: function(){
+		this.element.style.display = '';
 		return this;
 	}
 	
@@ -165,14 +179,14 @@ ART.VML.Group = new Class({
 	
 });
 
-// VML Shape Class
+// VML Base Shape Class
 
-ART.VML.Shape = new Class({
+ART.VML.Base = new Class({
 
 	Extends: ART.VML.Element,
 	
-	initialize: function(path){
-		this.parent('shape');
+	initialize: function(tag){
+		this.parent(tag);
 		var shape = this.shape = this.element;
 		var element = this.element = document.createElement('av:group');
 		element.appendChild(shape);
@@ -184,178 +198,10 @@ ART.VML.Shape = new Class({
 		var stroke = this.strokeElement = document.createElement('av:stroke');
 		stroke.on = false;
 		shape.appendChild(stroke);
-		
-		if (path != null) this.draw(path);
 	},
 	
 	getBBox: function(){
 		return {x: this.x || 0, y: this.y || 0, width: this.width || 0, height: this.height || 0};
-	},
-	
-	// SVG parser
-	
-	// Possible SVG syntaxes that we want to support
-	// M & L could be M L C Z m l c z
-	// separators can always be spaces, one comma, or any combination of spaces and one comma
-	// numbers can also be separated by a minus or plus sign. "100+100" means [100, 100], "100-100" means [100, -100]
-	
-	// Inputs Examples
-	
-	// 'M 100.5,-100.5 L 100,100'  //space separator for methods, comma separators for vectors
-	// 'M100.5,-100.5L100,100' //no separator for methods, comma separators for vectors
-	// 'M100.5-100.5L100,100' //comma separator for vectors when needed (Adobe Illustrator)
-	// 'M100.5-100.5L100 100' //space separator for vectors when needed
-	// 'M 100.5 -100.5 L 100 -100' //space separator (ART.Path)
-	// 'M 100.5, -100.5 L 100, 100' //space separator for methods, comma + space separators for vectors
-	// 'M 100.5-100.5 L 100.5,100.5' //space separator for methods, comma separators for vectors when needed
-	// 'M 100.5,-100.5 L 100,100' //space separator for methods, comma separators for vectors
-	
-	// Outoput
-	
-	// any output would do, as long as it provides separated parts. These are a couple of examples:
-	
-	// [['M', 100.5, -100.5], ['L', 100, -100]]
-	
-	// [{method: 'M': vectors: [100.5, -100.5]}, {method: 'L', vectors: [100, -100]}]
-	
-	draw: function(path){
-		this.boundsX = []; this.boundsY = [];
-		
-		path = path.toString().replace(/\s*([A-Za-z,-])\s*/ig, function(f, m, i){
-			switch (m){
-				case '-': return ' ' + m;
-				case ',': return ' ';
-				default: return ' ' + m + ' ';
-			}
-		});
-
-		var parts = [], index = -1, i, bits = path.split(/\s+/), p = precision, p2 = p / 2;
-		
-		for (i = 0; i < bits.length; i++){
-			var bit = bits[i], e;
-			if (bit.match(/[A-Za-z]/i)) parts[++index] = [bit];
-			else parts[index].push(Number(bit));
-		}
-		
-		path = '';
-		
-		var reflect = function(sx, sy, ex, ey){
-			return [ex * 2 - sx, ey * 2 - sy];
-		};
-		
-		var X = 0, Y = 0, px = 0, py = 0, r;
-		
-		for (i = 0; i < parts.length; i++){
-			var v = parts[i];
-			
-			switch (v.shift()){
-				
-				case 'm':
-					path += 'm' + this._ux(X += v[0]) + ',' + this._uy(Y += v[1]);
-				break;
-				case 'M':
-					path += 'm' + this._ux(X = v[0]) + ',' + this._uy(Y = v[1]);
-				break;
-				
-				case 'l':
-					path += 'l' + this._ux(X += v[0]) + ',' + this._uy(Y += v[1]);
-				break;
-				case 'L':
-					path += 'l' + this._ux(X = v[0]) + ',' + this._uy(Y = v[1]);
-				break;
-				
-				case 'c':
-					px = X + v[2]; py = Y + v[3];
-					path += 'c' + this._ux(X + v[0]) + ',' + this._uy(Y + v[1]) + ',' + this._ux(px) + ',' + 
-					this._uy(py) + ',' + this._ux(X += v[4]) + ',' + this._uy(Y += v[5]);
-				break;
-				case 'C':
-					px = v[2]; py = v[3];
-					path += 'c' + this._ux(v[0]) + ',' + this._uy(v[1]) + ',' + this._ux(px) + ',' +
-					this._uy(py) + ',' + this._ux(X = v[4]) + ',' + this._uy(Y = v[5]);
-				break;
-				
-				case 's':
-					r = reflect(px, py, X, Y);
-					px = X + v[0]; py = Y + v[1];
-					path += 'c' + this._ux(r[0]) + ',' + this._uy(r[1]) + ',' + this._ux(px) + ',' + 
-					this._uy(py) + ',' + this._ux(X += v[2]) + ',' + this._uy(Y += v[3]);
-				break;
-				case 'S':
-					r = reflect(px, py, X, Y);
-					px = v[0]; py = v[1];
-					path += 'c' + this._ux(r[0]) + ',' + this._uy(r[1]) + ',' + this._ux(px) + ',' + 
-					this._uy(py) + ',' + this._ux(X = v[2]) + ',' + this._uy(Y = v[3]);
-				break;
-				
-				case 'q':
-					px = X + v[0]; py = Y + v[1];
-					path += 'c' + this._ux(X + v[0]) + ',' + this._uy(Y + v[1]) + ',' + this._ux(px) + ',' + 
-					this._uy(py) + ',' + this._ux(X += v[2]) + ',' + this._uy(Y += v[3]);
-				break;
-				case 'Q':
-					px = v[0]; py = v[1];
-					path += 'c' + this._ux(v[0]) + ',' + this._uy(v[1]) + ',' + this._ux(px) + ',' +
-					this._uy(py) + ',' + this._ux(X = v[2]) + ',' + this._uy(Y = v[3]);
-				break;
-				
-				case 't':
-					r = reflect(px, py, X, Y);
-					px = X + r[0]; py = Y + r[1];
-					path += 'c' + this._ux(px) + ',' + this._uy(py) + ',' + this._ux(px) + ',' + 
-					this._uy(py) + ',' + this._ux(X += v[0]) + ',' + this._uy(Y += v[1]);
-				break;
-				case 'T':
-					r = reflect(px, py, X, Y);
-					px = r[0]; py = r[1];
-					path += 'c' + this._ux(px) + ',' + this._uy(py) + ',' + this._ux(px) + ',' + 
-					this._uy(py) + ',' + this._ux(X = v[0]) + ',' + this._uy(Y = v[1]);
-				break;
-				
-				case 'h':
-					path += 'l' + this._ux(X += v[0]) + ',' + this._uy(Y);
-				break;
-				case 'H':
-					path += 'l' + this._ux(X = v[0]) + ',' + this._uy(Y);
-				break;
-				
-				case 'v':
-					path += 'l' + this._ux(X) + ',' + this._uy(Y += v[0]);
-				break;
-				case 'V':
-					
-					path += 'l' + this._ux(X) + ',' + this._uy(Y = v[0]);
-				break;
-				
-				case 'z':
-					path += 'x';
-				break;
-				case 'Z':
-					path += 'x';
-				break;
-				
-			}
-		}
-		
-		this.width = Math.max.apply(Math, this.boundsX);
-		this.height = Math.max.apply(Math, this.boundsY);
-		this.x = Math.min.apply(Math, this.boundsX);
-		this.y = Math.min.apply(Math, this.boundsY);
-		
-		this._transform();
-
-		this.shape.path = path + 'e';
-		return this;
-	},
-	
-	_ux: function(x){
-		this.boundsX.push(x);
-		return Math.round(x * precision);
-	},
-	
-	_uy: function(y){
-		this.boundsY.push(y);
-		return Math.round(y * precision);
 	},
 	
 	/* transform */
@@ -409,8 +255,12 @@ ART.VML.Shape = new Class({
 		return this;
 	},
 	
-	stroke: function(flag){
+	stroke: function(flag, width, cap, join){
 		var stroke = this.strokeElement;
+		
+		stroke.weight = (width != null) ? width : 1;
+		stroke.endcap = (cap != null) ? ((cap == 'butt') ? 'flat' : cap) : 'round';
+		stroke.joinstyle = (join != null) ? join : 'round';
 
 		if (flag == null){
 			stroke.on = false;
@@ -420,6 +270,175 @@ ART.VML.Shape = new Class({
 			stroke.opacity = color[1];
 			stroke.on = true;
 		}
+		return this;
+	}
+
+});
+
+// VML Shape Class
+
+ART.VML.Shape = new Class({
+
+	Extends: ART.VML.Base,
+	
+	initialize: function(path){
+		this.parent('shape');
+		if (path != null) this.draw(path);
+	},
+	
+	// SVG parser
+	
+	// Possible SVG syntaxes that we want to support
+	// M & L could be M L C Z m l c z
+	// separators can always be spaces, one comma, or any combination of spaces and one comma
+	// numbers can also be separated by a minus or plus sign. "100+100" means [100, 100], "100-100" means [100, -100]
+	
+	// Inputs Examples
+	
+	// 'M 100.5,-100.5 L 100,100'  //space separator for methods, comma separators for vectors
+	// 'M100.5,-100.5L100,100' //no separator for methods, comma separators for vectors
+	// 'M100.5-100.5L100,100' //comma separator for vectors when needed (Adobe Illustrator)
+	// 'M100.5-100.5L100 100' //space separator for vectors when needed
+	// 'M 100.5 -100.5 L 100 -100' //space separator (ART.Path)
+	// 'M 100.5, -100.5 L 100, 100' //space separator for methods, comma + space separators for vectors
+	// 'M 100.5-100.5 L 100.5,100.5' //space separator for methods, comma separators for vectors when needed
+	// 'M 100.5,-100.5 L 100,100' //space separator for methods, comma separators for vectors
+	
+	// Outoput
+	
+	// any output would do, as long as it provides separated parts. These are a couple of examples:
+	
+	// [['M', 100.5, -100.5], ['L', 100, -100]]
+	
+	// [{method: 'M': vectors: [100.5, -100.5]}, {method: 'L', vectors: [100, -100]}]
+	
+	draw: function(path){
+		var boundsX = [], boundsY = [];
+		
+		var ux = function(x){
+			boundsX.push(x);
+			return Math.round(x * precision);
+		}, uy = function(y){
+			boundsY.push(y);
+			return Math.round(y * precision);
+		};
+		
+		path = path.toString().replace(/\s*([A-Za-z,-])\s*/ig, function(f, m, i){
+			switch (m){
+				case '-': return ' ' + m;
+				case ',': return ' ';
+				default: return ' ' + m + ' ';
+			}
+		});
+
+		var parts = [], index = -1, i, bits = path.split(/\s+/), p = precision, p2 = p / 2;
+		
+		for (i = 0; i < bits.length; i++){
+			var bit = bits[i], e;
+			if (bit.match(/[A-Za-z]/i)) parts[++index] = [bit];
+			else parts[index].push(Number(bit));
+		}
+		
+		path = '';
+		
+		var reflect = function(sx, sy, ex, ey){
+			return [ex * 2 - sx, ey * 2 - sy];
+		};
+		
+		var X = 0, Y = 0, px = 0, py = 0, r;
+		
+		for (i = 0; i < parts.length; i++){
+			var v = parts[i];
+			
+			switch (v.shift()){
+				
+				case 'm':
+					path += 'm' + ux(X += v[0]) + ',' + uy(Y += v[1]);
+				break;
+				case 'M':
+					path += 'm' + ux(X = v[0]) + ',' + uy(Y = v[1]);
+				break;
+				
+				case 'l':
+					path += 'l' + ux(X += v[0]) + ',' + uy(Y += v[1]);
+				break;
+				case 'L':
+					path += 'l' + ux(X = v[0]) + ',' + uy(Y = v[1]);
+				break;
+				
+				case 'c':
+					px = X + v[2]; py = Y + v[3];
+					path += 'c' + ux(X + v[0]) + ',' + uy(Y + v[1]) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X += v[4]) + ',' + uy(Y += v[5]);
+				break;
+				case 'C':
+					px = v[2]; py = v[3];
+					path += 'c' + ux(v[0]) + ',' + uy(v[1]) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X = v[4]) + ',' + uy(Y = v[5]);
+				break;
+				
+				case 's':
+					r = reflect(px, py, X, Y);
+					px = X + v[0]; py = Y + v[1];
+					path += 'c' + ux(r[0]) + ',' + uy(r[1]) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X += v[2]) + ',' + uy(Y += v[3]);
+				break;
+				case 'S':
+					r = reflect(px, py, X, Y);
+					px = v[0]; py = v[1];
+					path += 'c' + ux(r[0]) + ',' + uy(r[1]) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X = v[2]) + ',' + uy(Y = v[3]);
+				break;
+				
+				case 'q':
+					px = X + v[0]; py = Y + v[1];
+					path += 'c' + ux(X + v[0]) + ',' + uy(Y + v[1]) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X += v[2]) + ',' + uy(Y += v[3]);
+				break;
+				case 'Q':
+					px = v[0]; py = v[1];
+					path += 'c' + ux(v[0]) + ',' + uy(v[1]) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X = v[2]) + ',' + uy(Y = v[3]);
+				break;
+				
+				case 't':
+					r = reflect(px, py, X, Y);
+					px = X + r[0]; py = Y + r[1];
+					path += 'c' + ux(px) + ',' + uy(py) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X += v[0]) + ',' + uy(Y += v[1]);
+				break;
+				case 'T':
+					r = reflect(px, py, X, Y);
+					px = r[0]; py = r[1];
+					path += 'c' + ux(px) + ',' + uy(py) + ',' + ux(px) + ',' + uy(py) + ',' + ux(X = v[0]) + ',' + uy(Y = v[1]);
+				break;
+				
+				case 'h':
+					path += 'l' + ux(X += v[0]) + ',' + uy(Y);
+				break;
+				case 'H':
+					path += 'l' + ux(X = v[0]) + ',' + uy(Y);
+				break;
+				
+				case 'v':
+					path += 'l' + ux(X) + ',' + uy(Y += v[0]);
+				break;
+				case 'V':
+					
+					path += 'l' + ux(X) + ',' + uy(Y = v[0]);
+				break;
+				
+				case 'z':
+					path += 'x';
+				break;
+				case 'Z':
+					path += 'x';
+				break;
+				
+			}
+		}
+		
+		this.width = Math.max.apply(Math, boundsX);
+		this.height = Math.max.apply(Math, boundsY);
+		this.x = Math.min.apply(Math, boundsX);
+		this.y = Math.min.apply(Math, boundsY);
+		
+		this._transform();
+
+		this.shape.path = path + 'e';
 		return this;
 	}
 
