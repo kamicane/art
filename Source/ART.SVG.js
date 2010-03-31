34bfa640e330195229b730466bf86ba08b36c025
Sebastian Markbage <sebastian@calyptus.eu>
2010-03-31 03:56:54 +0200

    Implemented VML scaling and rotation (transform rewrite) - Compatible with SVG transform order: translate, rotate, scale - Supports nested groups. - Specifying width/height style ensures nested coordinate spaces. No need to access container. - Positioning using left/top style ensures correct boxing for CSS properties and external tools.

--- Source/ART.SVG.js
+++ Source/ART.SVG.js
@@ -59,7 +59,7 @@ ART.SVG.Element = new Class({
 		this.uid = (UID++).toString(16);
 		var element = this.element = createElement(tag);
 		element.setAttribute('id', 'e' + this.uid);
-		this.transform = {translate: [0, 0], scale: [1, 1], rotate: [0, 0, 0]};
+		this.transform = {translate: [0, 0], rotate: [0, 0, 0], scale: [1, 1]};
 	},
 	
 	/* transforms */
@@ -70,15 +70,15 @@ ART.SVG.Element = new Class({
 		this.element.setAttribute('transform', transforms.join(' '));
 	},
 	
-	// rotate: function(deg, x, y){
-	// 	if (x == null || y == null){
-	// 		var box = this.measure();
-	// 		x = box.x + box.width / 2; y = box.y + box.height / 2;
-	// 	}
-	// 	this.transform.rotate = [deg, x, y];
-	// 	this._writeTransform();
-	// 	return this;
-	// },
+	rotate: function(deg, x, y){
+		if (x == null || y == null){
+			var box = this.measure();
+			x = box.left + box.width / 2; y = box.top + box.height / 2;
+		}
+		this.transform.rotate = [deg, x, y];
+		this._writeTransform();
+		return this;
+	},
 
 	scale: function(x, y){
 		this.transform.scale = [x, y];
