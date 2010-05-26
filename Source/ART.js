989816f40706784bb89196e3891ca4b0d43563e9
Valerio Proietti <kamicane@gmail.com>
2010-05-26 16:14:21 +0200

    - Versions should be major - minor - revision. Sorry about that :)

--- Source/ART.js
+++ Source/ART.js
@@ -11,7 +11,7 @@ provides: [ART, ART.Element, ART.Container]
 
 this.ART = new Class;
 
-ART.version = '0.87';
+ART.version = '0.8.7';
 ART.build = '37db3609c6e0df4c215737df9c3c5851d1e9c30c';
 
 ART.Element = new Class({
