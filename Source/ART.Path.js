67519f8f8cb3d4dd800790ddcee9142bb480464a
Sebastian Markbage <sebastian@calyptus.eu>
2011-01-10 21:46:16 +0100

    Allow measurement of path without lines

--- Source/ART.Path.js
+++ Source/ART.Path.js
@@ -189,6 +189,8 @@ var extrapolate = function(parts, precision){
 		}
 	}
 	
+	if (!boundsX.length) return [path, {left: 0, top: 0, right: 0, bottom: 0, width: 0, height: 0}];
+	
 	var right = Math.max.apply(Math, boundsX),
 		bottom = Math.max.apply(Math, boundsY),
 		left = Math.min.apply(Math, boundsX),
